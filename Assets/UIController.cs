123c8b2ab9428b64cf56ff36c10120fc131ecd9a
valbeat <kj1ktk@gmail.com>
2015-01-25 03:31:52 +0900

    [WIP]ゲーム画面のUIの実装

---
+++ Assets/UIController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+	public GameObject menu = null;
+
+	void ShowMenu () {
+		Debug.Log("true");
+		menu.SetActive(true);
+	}
+}
